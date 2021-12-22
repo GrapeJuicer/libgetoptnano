402f9b95e29cf3462e2f843ed7126f52aa949784
GrapeJuicer <Dev.Grape@outlook.jp>
2021-12-22 13:24:54 +0900

    [add] initial commit

---
+++ example/example.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "getoptnano.h"
+
+int main(void)
+{
+    printf("Hello, getoptnano!\n");
+
+    return 0;
+}
---
+++ include/getoptnano.h
@@ -0,0 +1,34 @@
+// cpp でのインクルードガード
+
+#ifndef _GETOPTNANO_H
+#define _GETOPTNANO_H 1
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+// for C++ -----------------------------
+extern "C"
+{
+#endif // __cplusplus
+
+// argument
+#ifndef _GETOPT_H
+#define no_argument 0
+#define required_argument 1
+#define optional_argument 2
+#endif
+
+// -------------------------------------
+
+
+
+
+
+// -------------------------------------
+
+#ifdef __cplusplus
+// for C++ -----------------------------
+}
+#endif // __cplusplus
+
+#endif // GETOPTNANO_H
